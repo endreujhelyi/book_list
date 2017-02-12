d790d1b5641443b396b6d3ba42d40410f9736dcb
Endre <endreujhelyi@gmail.com>
2017-02-12 23:42:25 +0100

    added activeBook reducer

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">{book.title}
+        </li>
       );
     });
   }
@@ -21,13 +27,15 @@ class BookList extends Component {
 }
 
 
-function mapStateToProps(state) {
-  // Whatever is returned will show up as props inside of
-  // Booklist
+const mapStateToProps = state => {
   return {
     books: state.books
   };
-}
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ selectBook }, dispatch);
+};
 
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
